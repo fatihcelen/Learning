eed06d844cbe9c52e40357ebfa4c6de0363667c7
Fatih Çelen <fatihcelen@gmail.com>
2019-09-29 23:15:24 +0300

    owin

---
+++ AspNetCore/CodeExamples/Web.OWIN/IsMantigi.cs
@@ -0,0 +1,31 @@
+﻿ 
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+ 
+namespace Web.OWIN
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public class IsMantigi
+    {
+        private readonly AppFunc birSonrakiKatman;
+
+        public IsMantigi(AppFunc birSonrakiKatman)
+        {
+            if (birSonrakiKatman == null)
+            {
+                throw new ArgumentNullException("birSonrakiKatman");
+            }
+
+            this.birSonrakiKatman = birSonrakiKatman;
+        }
+
+        public Task Invoke(IDictionary<string, object> environment)
+        {
+            System.Diagnostics.Trace.WriteLine("İstek iş mantığınca işleniyor");
+
+            return birSonrakiKatman(environment);
+        }
+    }
+}
