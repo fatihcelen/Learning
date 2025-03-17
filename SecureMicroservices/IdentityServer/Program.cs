707bf9cd78d990a4472568bfd817126f89797043
Fatih ÇELEN <fatihcelen@gmail.com>
2025-03-17 06:47:42 +0300

    add inmemory db for identity

--- SecureMicroservices/IdentityServer/Program.cs
+++ SecureMicroservices/IdentityServer/Program.cs
@@ -1,6 +1,15 @@
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddIdentityServer();
+builder.Services.AddIdentityServer()
+    .AddInMemoryClients(new List<Client>())
+    .AddInMemoryApiScopes(new List<ApiScope>())
+    .AddInMemoryIdentityResources(new List<IdentityResource>())
+    .AddInMemoryApiResources(new List<ApiResource>())
+    .AddTestUsers(new List<TestUser>())
+    .AddDeveloperSigningCredential();;
 
 var app = builder.Build();
 
